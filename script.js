0ef0c5ac27c773a3c7ebf0c998ebbbf67edfaf0a
Nimira43 <n.rabone72@aol.com>
2025-04-28 23:23:10 +0100

    Getting ready to create a number of JS variables

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const mainPage = document.querySelector('.main-page')
 const loginPage = document.querySelector('.login-page')
 const middleContent = document.querySelector('.middle-content')
 
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+const btnTop = document.querySelector('.btn-top')
+
 const goToLoginPage = () => {
   mainPage.style.display = 'none'
   loginPage.style.display = 'grid'
