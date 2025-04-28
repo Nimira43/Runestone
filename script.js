36e698d810694cd8434d8235fe6c4db5983318cf
Nimira43 <n.rabone72@aol.com>
2025-04-28 22:26:04 +0100

    goToLoginPAge function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const mainPage = document.querySelector('.main-page')
 const loginPage = document.querySelector('.login-page')
 const middleContent = document.querySelector('.middle-content')
+
+const goToLoginPage = () => {
+  mainPage.style.display = 'none'
+  loginPage.style.display = 'grid'
+}
