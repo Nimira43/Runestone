b80b11ba504bad3aff4fc9bcf38c5da7c20d15cf
Nimira43 <n.rabone72@aol.com>
2025-05-25 01:23:11 +0100

    Added login-user-info and login-password classes in HTML.

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ loginFormBtn.addEventListener('click', () => {
   const loginUserInfo = document.querySelector('.login-user-info')
   const loginPassword = document.querySelector('.login-password')
 
-  if(loginUserInfo.value !== '' && loginPassword.value !== '') {
+  if (loginUserInfo.value !== '' && loginPassword.value !== '') {
     loginPage.style.display = 'none'
     newsFeedPage.style.display = 'block'
   } else {
