dd8fe0f042a150ad723b3ea8150ade9799cbb7cb
Nimira43 <n.rabone72@aol.com>
2025-05-25 01:13:38 +0100

    Login form button script.

--- script.js
+++ script.js
@@ -48,3 +48,15 @@ btnTop.addEventListener('click', () => {
 modalX.addEventListener('click', () => {
   loginModal.style.display = 'none'
 })
+
+loginFormBtn.addEventListener('click', () => {
+  const loginUserInfo = document.querySelector('.login-user-info')
+  const loginPassword = document.querySelector('.login-password')
+
+  if(loginUserInfo.value !== '' && loginPassword.value !== '') {
+    loginPage.style.display = 'none'
+    newsFeedPage.style.display = 'block'
+  } else {
+    loginModal.style.display = 'block'
+  }
+})
